a0dbfd1aa021b89b576724bada7257e6d3bf2eeb
ChemikPIL <chemikpil@gmail.com>
2014-07-10 23:48:11 +0200

    Auth user

--- controllers/sign-in/index.js
+++ controllers/sign-in/index.js
@@ -1,18 +1,28 @@
 'use strict';
 
 
-var UserModel = require('../../models/user');
+var SignInModel = require('../../models/sign-in');
+var passport = require('passport');
 
 
 module.exports = function (router) {
 
-    var model = new UserModel();
+  var model = new SignInModel();
 
-
-    router.get('/', function (req, res) {
+  router.get('/', function (req, res) {
+    
+    res.render('sign-in/index', model);
         
-        res.render('sign-in/index', model);
-        
-    });
+  });
+  
+  router.post('/', function (req, res) {
+    
+    passport.authenticate('local-login', {
+      successRedirect: req.session.goingTo || '/profile',
+      failureRedirect: '/sign-in',
+      faulereFlash: true
+    })(req, res);
+    
+  });
 
 };
