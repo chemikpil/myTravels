22532285b7d0e7d9ddc0ca06e3bf9f218a5e6d7e
ChemikPIL <chemikpil@gmail.com>
2014-07-20 19:35:25 +0200

    Testing login controller

--- test/sign-in/index.js
+++ test/sign-in/index.js
@@ -3,44 +3,196 @@
 'use strict';
 
 
-var kraken = require('kraken-js'),
-    express = require('express'),
-    request = require('supertest'),
-    spec = require('../../lib/spec');
+var kraken = require('kraken-js');
+var express = require('express');
+var request = require('supertest');
+var spec = require('../../lib/spec');
+
+var chai = require('chai');
+var should = chai.should();
+var mongoose = require('mongoose');
+var mockgoose = require('mockgoose');
+
+mockgoose(mongoose);
+
+var User = mongoose.model('User');
+var ObjectId = mongoose.Types.ObjectId();
+var user = null;
 
 
 describe('/sign-in', function () {
 
-    var app, mock;
+  var app, mock;
 
+  beforeEach(function (done) {
+    app = express();
+    app.on('start', done);
+    app.use(kraken({
+      basedir: process.cwd(),
+      onconfig: spec(app).onconfig
+    }));
+    
+    app.all('/profile', function (req, res) {
+      res.send(200, 'Profile page');
+    });
+    
+    mock = app.listen(1337);
+    
+    mockgoose.reset();
 
-    beforeEach(function (done) {
-        app = express();
-        app.on('start', done);
-        app.use(kraken({
-            basedir: process.cwd(),
-            onconfig: spec(app).onconfig
-        }));
-
-        mock = app.listen(1337);
-
+    user = new User({
+      _id: ObjectId,
+      email: 'test@mytravels.com',
+      password: '12345678',
+      name: 'Thomas',
+      surname: 'Tester',
+      trips: 0,
+      role: 'user',
+      registered: new Date(),
+      confirm_token: '12341234512345',
+      confirmed: true
     });
 
+    user.save();
+  });
 
-    afterEach(function (done) {
-        mock.close(done);
+
+  afterEach(function (done) {
+    mockgoose.reset();
+    mock.close(done);
+  });
+
+
+  it('should say "Welcome back"', function (done) {
+      
+    request(mock)
+      .get('/sign-in')
+      .expect(200)
+      .expect('Content-Type', /html/)
+      .expect(/Sign in/)
+      .end(function (err, res) {
+        done(err);
     });
-
-
-    it('should say "Welcome back"', function (done) {
+  });
+  
+  it('should redirect to /profile if the form is valid', function (done) {
+      
+    request(mock)
+      .get('/sign-in')
+      .end(function (err, res) {
+        var csrf = /value="(.*?)"/.exec(res.text)[1];
+        
         request(mock)
-            .get('/sign-in')
-            .expect(200)
-            .expect('Content-Type', /html/)
-            .expect(/Welcome back/)
-            .end(function (err, res) {
-                done(err);
-            });
+          .post('/sign-in')
+          .set('cookie', res.headers['set-cookie'])
+          .send({
+            _csrf: csrf,
+            email: 'test@mytravels.com',
+            password: '12345678'
+          })
+          .redirects(1)
+          .expect(200)
+          .expect(/Profile page/)
+          .end(function (err, res) {
+            done(err);
+          });
     });
+  });
+  
+  it('should redirect to /sign-in if the user has entered the wrong password', function (done) {
+      
+    request(mock)
+      .get('/sign-in')
+      .end(function (err, res) {
+        var csrf = /value="(.*?)"/.exec(res.text)[1];
+        
+        request(mock)
+          .post('/sign-in')
+          .set('cookie', res.headers['set-cookie'])
+          .send({
+            _csrf: csrf,
+            email: 'test@mytravels.com',
+            password: '12345677'
+          })
+          .redirects(1)
+          .expect(200)
+          .expect(/Sign in/)
+          .end(function (err, res) {
+            done(err);
+          });
+    });
+  });
+  
+  it('should redirect to /sign-in if the user has entered the wrong email', function (done) {
+      
+    request(mock)
+      .get('/sign-in')
+      .end(function (err, res) {
+        var csrf = /value="(.*?)"/.exec(res.text)[1];
+        
+        request(mock)
+          .post('/sign-in')
+          .set('cookie', res.headers['set-cookie'])
+          .send({
+            _csrf: csrf,
+            email: 'test2@mytravels.com',
+            password: '12345678'
+          })
+          .redirects(1)
+          .expect(200)
+          .expect(/Sign in/)
+          .end(function (err, res) {
+            done(err);
+          });
+    });
+  });
+  
+  it('should redirect to /sign-in if the user has not specified email and password', function (done) {
+      
+    request(mock)
+      .get('/sign-in')
+      .end(function (err, res) {
+        var csrf = /value="(.*?)"/.exec(res.text)[1];
+        
+        request(mock)
+          .post('/sign-in')
+          .set('cookie', res.headers['set-cookie'])
+          .send({
+            _csrf: csrf
+          })
+          .redirects(1)
+          .expect(200)
+          .expect(/Sign in/)
+          .end(function (err, res) {
+            done(err);
+          });
+    });
+  });
+  
+  it('should redirect to /sign-in if the user is not confirmed', function (done) {
+    user.confirmed = false;
+    user.save();
+    
+    request(mock)
+      .get('/sign-in')
+      .end(function (err, res) {
+        var csrf = /value="(.*?)"/.exec(res.text)[1];
+        
+        request(mock)
+          .post('/sign-in')
+          .set('cookie', res.headers['set-cookie'])
+          .send({
+            _csrf: csrf,
+            email: 'test@mytravels.com',
+            password: '12345678'
+          })
+          .redirects(1)
+          .expect(200)
+          .expect(/Sign in/)
+          .end(function (err, res) {
+            done(err);
+          });
+    });
+  });
 
 });
