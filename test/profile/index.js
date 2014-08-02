5d854ae48b35c784a2cbf6407a4edcf74b039457
ChemikPIL <chemikpil@gmail.com>
2014-08-02 22:43:02 +0200

    PProfile controller test

--- test/profile/index.js
+++ test/profile/index.js
@@ -3,42 +3,72 @@
 'use strict';
 
 
-var kraken = require('kraken-js'),
-    express = require('express'),
-    request = require('supertest');
+var kraken = require('kraken-js');
+var express = require('express');
+var request = require('supertest');
+var spec = require('../../lib/spec');
+
+var chai = require('chai');
+var should = chai.should();
+var mongoose = require('mongoose');
+var mockgoose = require('mockgoose');
+
+mockgoose(mongoose);
+
+var User = mongoose.model('User');
+var ObjectId = mongoose.Types.ObjectId();
+var user = null;
 
 
 describe('/profile', function () {
 
-    var app, mock;
+  var app, mock;
 
 
-    beforeEach(function (done) {
-        app = express();
-        app.on('start', done);
-        app.use(kraken({
-            basedir: process.cwd()
-        }));
+  beforeEach(function (done) {
+    app = express();
+    app.on('start', done);
+    app.use(kraken({
+      basedir: process.cwd(),
+      onconfig: spec(app).onconfig
+    }));
 
-        mock = app.listen(1337);
+    mock = app.listen(1337);
 
+    mockgoose.reset();
+
+    user = new User({
+      _id: ObjectId,
+      email: 'test@mytravels.com',
+      password: '12345678',
+      name: 'Thomas',
+      surname: 'Tester',
+      trips: 0,
+      role: 'user',
+      registered: new Date(),
+      confirm_token: '12341234512345',
+      confirmed: true
     });
 
-
-    afterEach(function (done) {
-        mock.close(done);
-    });
+    user.save();
+  });
 
 
-//    it('should say "hello"', function (done) {
-//        request(mock)
-//            .get('/profile')
-//            .expect(200)
-//            .expect('Content-Type', /html/)
-//            .expect(/Hello, /)
-//            .end(function (err, res) {
-//                done(err);
-//            });
-//    });
+  afterEach(function (done) {
+    mockgoose.reset();
+    mock.close(done);
+  });
+
+  it('should redirect to /sign-in if user is not logged', function (done) {
+    request(mock)
+      .get('/profile')
+      .expect(200)
+      .redirects(2)
+      .expect('Content-Type', /html/)
+      .expect(/Sign in/)
+      .end(function (err, res) {
+        done(err);
+      });
+  });
 
 });
