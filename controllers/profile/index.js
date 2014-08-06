f1e356092708b635972b881c1cb9e92832334d08
ChemikPIL <chemikpil@gmail.com>
2014-08-06 19:27:32 +0200

    Styling profile cover

--- controllers/profile/index.js
+++ controllers/profile/index.js
@@ -5,13 +5,12 @@ var ProfileModel = require('../../models/profile');
 
 module.exports = function (router) {
 
-    var model = new ProfileModel();
+  var model = new ProfileModel();
 
+  router.get('/', function (req, res) {
 
-    router.get('/', function (req, res) {
-      
-      res.render('profile/index', model);
+    res.render('profile/index', model);
         
-    });
+  });
 
 };
