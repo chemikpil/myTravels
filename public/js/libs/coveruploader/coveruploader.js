0a5208c2afefdfad56caccf29d8bcd468e37de91
ChemikPIL <chemikpil@gmail.com>
2014-08-22 23:48:48 +0200

    Create first version Coveruploader lib

--- public/js/libs/coveruploader/coveruploader.js
+++ public/js/libs/coveruploader/coveruploader.js
@@ -1,3 +1,36 @@
 var Coveruploader = function (options) {
   'use strict';
+  
+  var element;
+  var url;
+  
+  var classes = {
+    active: 'is-active'
+  };
+  
+  var init = function () {
+    setOptions();
+    
+    element.addEventListener('dragenter', handleDragEnter);
+    element.addEventListener('dragleave', handleDragLeave);
+  };
+  
+  var setOptions = function () {
+    if (options && options.element && options.url) {
+      element = options.element;
+      url = options.url;
+    } else {
+      throw new Error("Missign options object") ;
+    }
+  };
+  
+  var handleDragEnter = function () {
+    element.classList.add(classes.active);
+  };
+  
+  var handleDragLeave = function () {
+    element.classList.remove(classes.active);
+  };
+  
+  init();
 };
