9bb5fb1560a7e47e36ba9e0735bca724bc693628
ChemikPIL <chemikpil@gmail.com>
2014-08-24 16:33:44 +0200

    Change route to profile

--- controllers/profile/index.js
+++ controllers/profile/index.js
@@ -9,34 +9,27 @@ module.exports = function (router) {
   var model = new ProfileModel();
 
   router.get('/', function (req, res) {    
-    model.profile = res.locals.user;
-    
-    if (res.locals.user) {
-      model.class = 'class=is-editor';
-    }
-    
-    res.render('profile/index', model);
+    res.redirect('/profile/' + res.locals.user._id);
   });
     
   router.get('/:id', function (req, res) {
-    var param = req.params.id;
-    var query = {};
-    
-    if (param.indexOf('.') > 0) {
-      param = param.replace('.', ' ');
-      query = {name: param};
+    if (res.locals.user) {
+      model.profile = res.locals.user;
+      model.class = 'class=is-editor';
+      res.render('profile/index', model);
     } else {
-      query = {_id: param};
+      var param = req.params.id;
+      var query = {_id: param};
+
+      UserModel.findOne(query, function (err, user) {
+        if (user) {
+          model.profile = JSON.parse(JSON.stringify(user));
+          res.render('profile/index', model);
+        } else {
+          res.redirect('/'); 
+        }
+      });
     }
-    
-    UserModel.findOne(query, function (err, user) {
-      if (user) {
-        model.profile = JSON.parse(JSON.stringify(user));
-        res.render('profile/index', model);
-      } else {
-        res.redirect('/'); 
-      }
-    });
   });
   
   router.post('/uploadCover', function (req, res) {    
