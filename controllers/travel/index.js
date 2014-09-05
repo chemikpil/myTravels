a1a225198deddf90c4a4c1691d78e506478ca4d7
ChemikPIL <chemikpil@gmail.com>
2014-09-05 21:29:16 +0200

    Add create travel method

--- controllers/travel/index.js
+++ controllers/travel/index.js
@@ -6,13 +6,41 @@ var TravelModel = require('../../models/travel');
 
 module.exports = function (router) {
 
-    var model = new TravelModel();
-
-
-    router.get('/', function (req, res) {
-        
-        res.render('travel/index', model);
-        
+  router.get('/', function (req, res) {
+    res.redirect('/');
+  });
+  
+  router.get('/add', function (req, res) {
+    if (!res.locals.user) {
+      req.session.goingTo = '/travel/add';
+      res.redirect('/sign-in');
+      return;
+    }
+    
+    var model = new TravelModel({
+      author: res.locals.user._id
     });
+    
+    model.save(function (err) {
+      res.redirect('/travel/' + model._id);
+    });
+  });
+  
+  router.get('/:id' , function (req, res) {
+    var param = req.params.id;
+    var query = {_id: param};
+    var model = {};
+    
+    TravelModel.findOne(query)
+    .populate('author')
+    .exec(function (err, travel) {
+      if (travel) {
+        model = JSON.parse(JSON.stringify(travel));
+        res.render('travel/index', model);
+      } else {
+        res.redirect('/'); 
+      }
+    });
+  });
 
 };
