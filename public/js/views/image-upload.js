cfbb90359c9884fac62cf538632b5947a9cb7ea2
ChemikPIL <chemikpil@gmail.com>
2014-10-10 19:00:57 +0200

    Send uploaded files

--- public/js/views/image-upload.js
+++ public/js/views/image-upload.js
@@ -19,16 +19,18 @@ define([
     
     initialize: function (options) {
       this.alt = options.alt;
+      this.parent = options.parent;
     },
     
     render: function () {
       var input = document.createElement('input');
       input.type = 'file';
       input.name = 'upload-image';
+      input.setAttribute('multiple', true);
       this.el.appendChild(input);
       
       var info = document.createElement('span');
-      info.textContent = 'Drag and drop photo here or click to upload';
+      info.textContent = 'Drag and drop photo here or click to upload. Max 3!';
       this.el.appendChild(info);
       
       return this;
@@ -50,7 +52,40 @@ define([
       this.el.classList.remove('is-drag');
     },
     
-    dropFile: function () {
+    dropFile: function (event) {
+      var files = event.target.files;
+      var formData = new FormData();
+      
+      if (files.length > 3) {
+        return false;
+      }
+      
+      for (var i = 0, l = files.length; i < l; i++) {
+        formData.append(event.target.name + '-' + i, files[i], files[i].name);
+      }
+      
+      this.sendFiles(formData);
+    },
+    
+    sendFiles: function (files) {
+      var xhr = new XMLHttpRequest();
+      var self = this;
+
+      xhr.open("POST", '/api/travel/' + this.parent.id + '/addimage', true);
+
+      xhr.onload = function(){
+        var error = false;
+        var content = xhr.responseText;
+
+        if (!error && (xhr.status >= 200 && xhr.status < 300)){
+          self.renderImage(content);
+        }
+      };
+
+      xhr.send(files);
+    },
+    
+    renderImage: function (files) {
       
     },
     
