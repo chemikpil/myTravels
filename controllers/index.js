d622ced8431c142b97d3c4d62e02edabf0939051
ChemikPIL <chemikpil@gmail.com>
2014-10-19 17:30:53 +0200

    Get last 10 travels

--- controllers/index.js
+++ controllers/index.js
@@ -1,15 +1,23 @@
 'use strict';
 
 
-var IndexModel = require('../models/index');
+var TravelModel = require('../models/travel');
 
 
 module.exports = function (router) {
-
-  var model = new IndexModel();
   
   router.get('/', function (req, res) {
-    res.render('index', model);
+    var query = {mode: 'active'};
+    
+    TravelModel.findOne(query)
+      .limit(10)
+      .sort({date: -1})
+      .exec(function (err, travels) {
+        res.render('index', {
+          travels: travels
+        });
+      });
+    
   });
   
   router.get('/logout', function (req, res) {
