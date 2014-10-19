3a77ce65c83436fd4ce8b0d7a7aa1e6f1b16b2c0
ChemikPIL <chemikpil@gmail.com>
2014-10-19 17:48:56 +0200

    Fix query

--- controllers/index.js
+++ controllers/index.js
@@ -10,13 +10,13 @@ module.exports = function (router) {
     var query = {mode: 'active'};
     var select = {title: 1, 'location.name': 1, url: 1, cover_photo: 1, _id: 0}
     
-    TravelModel.findOne(query)
+    TravelModel.find(query)
       .select(select)
       .limit(10)
       .sort({date: -1})
       .exec(function (err, travels) {
         res.render('index', {
-          travels: travels
+          travels: JSON.stringify(travels)
         });
       });
     
