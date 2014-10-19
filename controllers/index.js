fd6bfe86f4c4fef6e6f445b3a8595e01f32516a8
ChemikPIL <chemikpil@gmail.com>
2014-10-19 17:37:12 +0200

    Select fields

--- controllers/index.js
+++ controllers/index.js
@@ -8,8 +8,10 @@ module.exports = function (router) {
   
   router.get('/', function (req, res) {
     var query = {mode: 'active'};
+    var select = {title: 1, 'location.name': 1, url: 1, cover_photo: 1, _id: 0}
     
     TravelModel.findOne(query)
+      .select(select)
       .limit(10)
       .sort({date: -1})
       .exec(function (err, travels) {
